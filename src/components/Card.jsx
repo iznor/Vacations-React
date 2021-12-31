299ac6409c6e8a15a2994b9932b0f83bc9093f4e
iZnoR <76132625+iznor@users.noreply.github.com>
2021-12-31 17:12:21 +0200

    Card- delete, edit, location, h4, h5

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,10 +6,12 @@ function Card({ onClickEdit }) {
       <div className="card">
         <div className="photo"></div>
         <div className="buttons">
-          <button className="edit"></button>
           <button className="delete"></button>
+          <button className="edit"></button>
         </div>
-        <h1>Iby</h1>
+        <h4>Phi Phi Islands</h4>
+        <div className="marker"></div>
+        <h5>Thailand</h5>
         {/*sIbytates */}
         {/* <button onClick={() => onClickEdit && onClickEdit(5)}>edit</button> */}
       </div>
